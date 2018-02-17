97df50688af5e52d02aa831207942d7dc8114a53
Maged El-Najaar <33536718+MagedElNajaar@users.noreply.github.com>
2018-02-17 12:41:40 +0200

    Create ProfanityAlert.py

---
+++ Course 1/ProfanityEditor/ProfanityAlert.py
@@ -0,0 +1,18 @@
+import urllib
+
+def read_text():
+    quotes = open("E:\WORK&STUDY\project\Udicty FULL STACK\Course 1\Profanity_Editor\movie_quotes.txt")
+    contents_of_file = quotes.read()
+    print (contents_of_file)
+    quotes.close()
+    check_profanity(contents_of_file)
+    
+def check_profanity(text_to_check):
+    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q="+text_to_check)
+    output = connection.read()
+    connection.close()
+    if "true" in output:
+        print("Profanity Alert!!")
+    elif "false" in output:
+        print("No curse Words!")
+read_text()
