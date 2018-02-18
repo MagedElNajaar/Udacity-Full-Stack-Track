9384b71002d12d63d62cc17baf2fdd3749053593
Maged El-Najaar <33536718+MagedElNajaar@users.noreply.github.com>
2018-02-18 19:40:36 +0200

    Delete ReadfromText.py

--- Course 1/ProfanityEditor/ReadfromText.py
+++
@@ -1,9 +0,0 @@
-import urllib
-
-def read_text():
-    quotes = open("E:\WORK&STUDY\project\Udicty FULL STACK\Course 1\Profanity_Editor\movie_quotes.txt")
-    contents_of_file = quotes.read()
-    print (contents_of_file)
-    quotes.close()
-    
-read_text()
