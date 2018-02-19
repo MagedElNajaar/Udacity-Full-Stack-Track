3cdf9fa9bd9524773c65108b02b260c0d6375724
Maged El-Najaar <33536718+MagedElNajaar@users.noreply.github.com>
2018-02-19 13:15:21 +0200

    Update MovieWebSiteCreation.py

--- 1- Programming Foundations with Python/Movie Website Creation/MovieWebSiteCreation.py
+++ 1- Programming Foundations with Python/Movie Website Creation/MovieWebSiteCreation.py
@@ -179,12 +179,12 @@ class Movie():
 
 toy_story = Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
-                        "https://upload.wikimedia.org/Toy_Story.jpg",
+                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
 avatar = Movie("Avatar",
                      "A marine on an alien planet",
-                     "https://upload.wikimedia.org/Avatar-Teaser-Poster.jpg",
+                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 movies = [toy_story,avatar]
 open_movies_page(movies)
